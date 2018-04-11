261b61640c27a448fcca0d7350380344e61a84cf
xuwanjin <xuwanjin1991@Gmail.com>
2018-04-11 10:05:20 +0800

    read argments from config.txt
    Signed-off-by: xuwanjin <xuwanjin1991@Gmail.com>

--- AutoCompile/auto_compile.py
+++ AutoCompile/auto_compile.py
@@ -15,7 +15,7 @@ from SvnCheckoutThread import SvnCheckoutThread
 from util import execute_command
 
 REPO_NAME = "ALPS-MP-N1.MP18-V1_AUS6739_66_N1_INHOUSE/"
-svn_repo_root = "svn://192.168.29.29/repository/L05A/Trunk/ALPS-MP-N1.MP18-V1_AUS6739_66_N1_INHOUSE/"
+SVN_REPO_PATH = "svn://192.168.29.29/repository/L05A/Trunk/ALPS-MP-N1.MP18-V1_AUS6739_66_N1_INHOUSE/"
 svn_repo_module = "packages/apps/Settings"
 SVN_REPO_NOT_EXIST = "the repo doesn't exist"
 SVN_REPO_CHECKOUT_SUCCESSFUL = "svn repo checkout successfully"
@@ -27,7 +27,7 @@ ENG_TYPE = "eng"
 DEBUG_TYPE = "user_debug"
 USER_TYPE = "user"
 BUILD_LOG_FILE = 'build.txt'
-FULL_PROJECT_NAME = ""
+FULL_PROJECT_NAME = "5058A"
 
 FULL_PROJECT_OUT_DIR = "out/target/product/" + platform_name
 
@@ -56,6 +56,23 @@ def get_sys_args():
     return full_project_name, build_variant
 
 
+def read_args_from_config():
+    with open("config.txt", 'r') as config_file:
+        for line in config_file.readlines():
+            if line.__contains__("project_svn_path="):
+                global SVN_REPO_PATH
+                SVN_REPO_PATH = line.split("=")[-1].strip("\n")
+            if line.__contains__("build_type"):
+                global BUILD_VARIANT
+                BUILD_VARIANT = line.split("=")[-1].strip("\n")
+            if line.__contains__("project_name"):
+                global FULL_PROJECT_NAME
+                FULL_PROJECT_NAME = line.split("=")[-1].strip("\n")
+            if line.__contains__("repo_name"):
+                global REPO_NAME
+                REPO_NAME = line.split("=")[-1].strip("\n")
+
+
 def execute_all_command():
     # svn_checkout_command = "svn checkout %s" % svn_repo_root
     # execute_command(svn_checkout_command)
@@ -68,7 +85,7 @@ def execute_all_command():
     choose_sub_branch_command = "cd %s && ./choosebranch_auto.sh %s " % (REPO_NAME, sub_project_name)
 
     execute_command(choose_sub_branch_command)
-    choose_full_branch_command = "cd %s && ../choosebranch_auto.sh %s" % (REPO_NAME, FULL_PROJECT_NAME)
+    choose_full_branch_command = "cd %s && ./choosebranch_auto.sh %s" % (REPO_NAME, FULL_PROJECT_NAME)
 
     # ./choosebranch_auto.sh 5058I_ALAE
     execute_command(choose_full_branch_command)
@@ -98,28 +115,28 @@ def execute_all_command():
 
 
 def svn_checkout_parallel_with_thread():
-    status, result = commands.getstatusoutput("svn list %s" % svn_repo_root)
+    status, result = commands.getstatusoutput("svn list %s" % SVN_REPO_PATH)
     file_list = result.split("\n")
 
     for f in file_list:
         checkout_thread = SvnCheckoutThread(
-            "mkdir %s && cd %s && svn checkout %s/%s" % (REPO_NAME, REPO_NAME, svn_repo_root, f))
+            "mkdir %s && cd %s && svn checkout %s/%s" % (REPO_NAME, REPO_NAME, SVN_REPO_PATH, f))
         checkout_thread.setName(f)
         checkout_thread.start()
 
 
 def svn_checkout_parallel_with_map():
-    status, result = commands.getstatusoutput("svn list %s" % svn_repo_root)
+    status, result = commands.getstatusoutput("svn list %s" % SVN_REPO_PATH)
     file_list = result.split("\n")
     command_list = list()
     command_list_file = list()
-    command_list_file.append("svn checkout --depth=empty %s " % svn_repo_root + REPO_NAME)
+    command_list_file.append("svn checkout --depth=empty %s " % SVN_REPO_PATH + REPO_NAME)
 
     for f in file_list:
         if is_file(f):
             command_list_file.append('cd %s && svn update %s' % (REPO_NAME, f))
         else:
-            command_list.append('cd %s && svn checkout %s%s' % (REPO_NAME, svn_repo_root, f))
+            command_list.append('cd %s && svn checkout %s%s' % (REPO_NAME, SVN_REPO_PATH, f))
 
     # svn update , will produce the svn lock file, so we need queue the update task
     for f in command_list_file:
@@ -132,12 +149,15 @@ def svn_checkout_parallel_with_map():
 
 
 def is_file(f):
-    status, result = commands.getstatusoutput("svn list  --depth=files %s" % svn_repo_root)
-    return result.__contains__(f)
+    status, result = commands.getstatusoutput("svn list  --depth=files %s" % SVN_REPO_PATH)
+    if status == 0:
+        return result.__contains__(f)
+    raise RuntimeError("svn list command execute exception")
 
 
 if __name__ == '__main__':
-    FULL_PROJECT_NAME, BUILD_VARIANT = get_sys_args()
+    # FULL_PROJECT_NAME, BUILD_VARIANT = get_sys_args()
+    read_args_from_config()
     # svn_checkout_parallel_with_thread()
     svn_checkout_parallel_with_map()
     execute_all_command()
